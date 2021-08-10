f2f88e18ebbe53edf34fb9962b8fd0d5b03a159a
nadrojmit <64118633+nadrojmit@users.noreply.github.com>
2021-08-10 11:53:21 +0930

    Update

--- loadPresentationJS.js
+++ loadPresentationJS.js
@@ -31,7 +31,7 @@ function loadScript(number, url)
     }
 }
 
-let scriptURL = 'https://cdn.jsdelivr.net/gh/nadrojmit/shared-js-files@v1.0/';
+let scriptURL = 'https://cdn.jsdelivr.net/gh/nadrojmit/shared-js-files@v1.02/';
 let JStoLoad = {
     1: scriptURL+'rootAPI.js',
     2: scriptURL+'reveals.js',
