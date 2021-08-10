5c5f766f5505d0be1c254d0d0d6c0a6d8a9d0912
nadrojmit <64118633+nadrojmit@users.noreply.github.com>
2021-08-10 12:05:06 +0930

    versioning

--- loadPresentationJS.js
+++ loadPresentationJS.js
@@ -5,12 +5,12 @@ jQuery(document).ready(function() {
 
 let totalScripts = 0;
 let loadedScripts = 0;
-let requiredScripts = 7;
+let requiredScripts = 6;
 
 // load script function
 function loadScript(number, url)
 {
-    if(totalScripts<7){
+    if(totalScripts<6){
     // Adding the script tag to the head as suggested before
     totalScripts++;
     var head = document.head;
@@ -31,29 +31,17 @@ function loadScript(number, url)
     }
 }
 
-let scriptURL = 'https://cdn.jsdelivr.net/gh/nadrojmit/shared-js-files@v1.2/';
+let scriptURL = 'https://cdn.jsdelivr.net/gh/nadrojmit/shared-js-files@v1.21/';
 let JStoLoad = {
     1: scriptURL+'rootAPI.js',
     2: scriptURL+'reveals.js',
     3: scriptURL+'audio.js',
     4: scriptURL+'progress.js',
-    5: scriptURL+'loading-bar/loading-bar.min.js',
-    6: scriptURL+'initialisePresentation.js',
-    7: scriptURL+"navigation.js"
+    5: scriptURL+'initialisePresentation.js',
+    6: scriptURL+"navigation.js"
 }
 
 function loadPresentationScripts() {
-    loadScript(0,"../../js/core/presentation/navigation.js");
-    /*jQuery.each(JStoLoad, function(i, val) {
-        loadScript(i,val);    
-    });
-    /*console.log("loadPresentationScripts: Function called");
-    loadScript('../../js/core/presentation/navigation.js');
-    loadScript('../../js/core/presentation/reveals.js');
-    loadScript('../../js/core/presentation/audio.js');
-    loadScript('../../js/core/presentation/rootAPI.js');
-    loadScript('../../js/core/presentation/progress.js');
-    loadScript('../../js/core/presentation/loading-bar/loading-bar.min.js');
-    loadScript('../../js/core/presentation/initialisePresentation.js');
-    console.log("loadPresentationScripts: Core scripts loaded");*/
+    loadScript(0,scriptURL+"navigation.js");
+
 }
